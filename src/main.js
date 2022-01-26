5e1ed8f11be28d036b79562079db87b1eb3e4cb8
Dan Gee <dangee1705@gmail.com>
2022-01-26 15:44:19 +0000

    added pug

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 let express = require("express");
+let pug = require("pug");
 let app = express();
 
 app.use("/css", express.static("./css"));
 app.use("/js", express.static("./js"));
+app.set("view engine", "pug");
 
 app.listen(8080);
