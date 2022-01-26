654d666bad3fe1dc0b4aa90d8493f45b9aab91a1
Jack Demonakis <jackdemonakis@gmail.com>
2022-01-26 16:12:05 +0000

    adding boilerplate for various pages

--- src/main.js
+++ src/main.js
@@ -8,4 +8,32 @@ app.set("view engine", "pug");
 app.get("/", (req, res) => res.render("index"));
 app.get("/about", (req, res) => res.render("about"));
 
+app.get("/pathwayselect", (req, res) => {
+	res.send("Pathway Selection");
+});
+
+app.get("/genreselect", (req, res) => {
+	res.send("Genre Selection");
+});
+
+app.get("/genres", (req, res) => {
+	res.send("Genres");
+});
+
+app.get("/projectselection", (req, res) => {
+	res.send("Project Selection");
+});
+
+app.get("/project/:id", (req, res) => {
+	res.send("Project overview with ID: "+req.params.id);
+});
+
+app.get("/studentoverview", (req, res) => {
+	res.send("Student Overview");
+});
+
+app.get("/submission", (req, res) => {
+	res.send("Submission");
+});
+
 app.listen(8080);
