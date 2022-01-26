d95dd853f1666e612ea503a9c047bc609910090b
Dan Gee <dangee1705@gmail.com>
2022-01-26 15:59:39 +0000

    added index and about page

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 let express = require("express");
-let pug = require("pug");
 let app = express();
 
 app.use("/css", express.static("./css"));
 app.use("/js", express.static("./js"));
 app.set("view engine", "pug");
+
+app.get("/", (req, res) => res.render("index"));
+app.get("/about", (req, res) => res.render("about"));
 
 app.listen(8080);
