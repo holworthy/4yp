b825b02523a663636a318468d4a41a92aef3a810
Dan Gee <dangee1705@gmail.com>
2022-02-01 12:19:34 +0000

    added user class

--- src/main.js
+++ src/main.js
@@ -1,13 +1,64 @@
+// imports
+
 let express = require("express");
 let app = express();
 let sqlite3 = require("sqlite3").verbose();
 
+
+// table definitions
+
 let db = new sqlite3.Database("database.db");
-db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, nickname TEXT, email TEXT)");
-db.run("CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY AUTOINCREMENT, userID INTEGER, campusCardNumber TEXT, threeTwoThree TEXT, FOREIGN KEY (userID) REFERENCES users (id))");
-db.run("CREATE TABLE IF NOT EXISTS supervisor (id INTEGER PRIMARY KEY AUTOINCREMENT, userId INTEGER, maxNumToSupervise INTEGER, FOREIGN KEY (userId) REFERENCES users(id))");
-db.run("CREATE TABLE IF NOT EXISTS admin (id INTEGER PRIMARY KEY AUTOINCREMENT, userID INTEGER, FOREIGN KEY (userID) REFERENCES users (id))");
-db.run("CREATE TABLE IF NOT EXISTS hubstaff (id INTEGER PRIMARY KEY AUTOINCREMENT, userId INTEGER, FOREIGN KEY (userId) REFERENCES users(id))");
+db.serialize(() => {
+	db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, nickname TEXT, email TEXT UNIQUE)");
+	db.run("CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY AUTOINCREMENT, userID INTEGER, campusCardNumber TEXT, threeTwoThree TEXT, FOREIGN KEY (userID) REFERENCES users (id))");
+	db.run("CREATE TABLE IF NOT EXISTS supervisor (id INTEGER PRIMARY KEY AUTOINCREMENT, userId INTEGER, maxNumToSupervise INTEGER, FOREIGN KEY (userId) REFERENCES users(id))");
+	db.run("CREATE TABLE IF NOT EXISTS admin (id INTEGER PRIMARY KEY AUTOINCREMENT, userID INTEGER, FOREIGN KEY (userID) REFERENCES users (id))");
+	db.run("CREATE TABLE IF NOT EXISTS hubstaff (id INTEGER PRIMARY KEY AUTOINCREMENT, userId INTEGER, FOREIGN KEY (userId) REFERENCES users(id))");
+	db.run("INSERT OR IGNORE INTO users(name, nickname, email) VALUES ('Bob Bobson', 'Bob', 'bob@example.com')");
+});
+
+
+// class definitions
+
+class User {
+	constructor(id, name, nickname, email) {
+		this.id = id;
+		this.name = name;
+		this.nickname = nickname;
+		this.email = email;
+	}
+
+	getId() {
+		return this.id;
+	}
+
+	getName() {
+		return this.name;
+	}
+
+	getNickname() {
+		return this.nickname;
+	}
+
+	getEmail() {
+		return this.email;
+	}
+
+	static getById(id, callback) {
+		let stmt = db.prepare("SELECT * FROM users WHERE id = ?", [id]);
+		stmt.get((err, row) => {
+			callback(new User(row.id, row.name, row.nickname, row.email));
+		});
+		stmt.finalize();
+	}
+}
+
+User.getById(1, user => {
+	console.log(user);
+});
+
+
+// web server
 
 app.use("/css", express.static("./css"));
 app.use("/js", express.static("./js"));
