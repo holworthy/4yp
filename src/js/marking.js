523f2aa93268c088dc2945d31e62e6e30246069f
Dan Gee <dangee1705@gmail.com>
2022-05-17 12:07:11 +0100

    finished marking

--- src/js/marking.js
+++ src/js/marking.js
@@ -1,14 +1,19 @@
 window.addEventListener("load", () => {
-	let partInputs = document.getElementsByClassName("part-input");
+	let rows = document.getElementsByClassName("part-row");
 	let submitMarkingButton = document.getElementById("submit-marking-button");
 	submitMarkingButton.addEventListener("click", () => {
 		let partData = [];
-		for(let i = 0; i < partInputs.length; i++)
-			partData.push([parseInt(partInputs[0].dataset.partId), parseFloat(partInputs[0].value)]);
+		for(let i = 0; i < rows.length; i++)
+			partData.push({
+				partId: parseInt(rows[i].dataset.partId),
+				mark: parseFloat(rows[i].getElementsByClassName("mark-input")[0].value),
+				feedback: rows[i].getElementsByTagName("textarea")[0].value
+			});
 		let xhr = new XMLHttpRequest();
 		xhr.open("POST", "/api/marking");
 		xhr.addEventListener("load", () => {
-
+			let result = JSON.parse(xhr.response);
+			location.href = "/marksheets/" + result.marksheetId;
 		});
 		xhr.addEventListener("error", () => alert("Could not submit marking"));
 		xhr.setRequestHeader("Content-Type", "application/json");
